6c1e536076c28a0a6be56a880e45b574ae8d5681
tentaciones <chideracharles987@gmail.com>
2023-02-27 02:36:44 +0100

    o

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -3,5 +3,14 @@ module.exports = {
     "react/display-name": "off",
   },
   parser: "@babel/eslint-parser",
-  extends: ["plugin:@next/next/recommended", ["eslint:recommended", "next"]],
+  extends: [
+    "plugin:@next/next/recommended",
+    ["eslint:recommended", "next"],
+    "next/core-web-vitals",
+  ],
+
+  env: {
+    browser: true,
+    es2021: true,
+  },
 };
