21cdf9c4f1ea390000ab99355074614bf09ffc85
tentaciones <chideracharles987@gmail.com>
2023-02-27 01:55:06 +0100

    fixed an error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+// eslint-disable-next-line react/display-name
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
